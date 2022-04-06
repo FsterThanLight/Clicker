23ff29c9f67ce5400574093d03901fb3b2db163b
federalsadler@sohu.com <federalsadler@sohu.com>
2022-04-06 11:27:42 +0800

    2022/4/6 完成初步窗体设计，及图片加载

--- 自动化点击器.py
+++ 自动化点击器.py
@@ -1,2 +1,26 @@
 import pyautogui
 import pyperclip
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from 窗体.add_instruction import Ui_Dialog
+from 窗体.mainwindow import Ui_MainWindow
+import sys
+
+
+class Main_window(QMainWindow, Ui_MainWindow):
+    """继承窗体的类，实现窗体和逻辑代码分离"""
+
+    def __init__(self):
+        super().__init__()
+        # 初始化窗体
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    main_window = Main_window()
+    # 给窗体程序设置图标
+    main_window.setWindowIcon(QIcon('图标.ico'))
+    # 显示窗体
+    main_window.show()
+    sys.exit(app.exec_())
